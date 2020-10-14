91c6e2a53edcc5b023c6e7b2a5b499e592c2ef68
hassanaref <hassan.aref159@gmail.com>
2020-10-14 22:42:13 +0200

    a

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ sections.forEach(sections => {
     secLink.setAttribute("href" , `#${sections.id}`)
     secLink.setAttribute("id" , `#${sections.id}L`)
     secLink.setAttribute("class" , `navEl`)
+    // secLink.setAttribute("class" , `navEl`)
         // append all created Li elements
     secLink.textContent = sections.firstElementChild.textContent
     const apLi = document.querySelector('#nav-bar').appendChild(secLink)
@@ -16,26 +17,36 @@ sections.forEach(sections => {
 
 
         //highlighting viewed section by observer
-
+let navL = Array.from(document.querySelectorAll('.navEl'));
+        console.log(navL)
         let options = {
             root: null,
-            rootMargin: '-360px',
-            threshold: 1
+            rootMargin: '150px',
+            threshold: 0.85
           }
                     
-        var observer = new IntersectionObserver(function(entries,observer){
+        let observer = new IntersectionObserver(function(entries,observer){
             console.log(entries)
             
             entries.forEach( entry => {
                 if(entry.isIntersecting){
                     entry.target.classList.add("active");
+                    navL.forEach( nav => {
+                        if (nav.outerText == entry.target.dataset.nav){
+                            nav.classList.add("activeNav")
+                        }
+                    })
                 } else {
                     entry.target.classList.remove("active");
+                    navL.forEach( nav =>{
+                        nav.classList.remove("activeNav")
+                    })
                 }
             });
         
         }, options);
 
+        
 sections.forEach( section => {
 
     observer.observe(section);
