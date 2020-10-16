31c0e8026ea0ff4331b9cd5f8949e7c7f1924ecd
hassanaref <hassan.aref159@gmail.com>
2020-10-16 19:11:48 +0200

    ل

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
         //selecting all sections
-
 const sections = Array.from(document.querySelectorAll('.sections'));
    console.log(`there is ${sections.length} sections`);
     console.log(sections)
         //loop sections to nav bar
 sections.forEach(sections => {  
     const secLink = document.createElement('a');
-    secLink.setAttribute("href" , `#${sections.id}`)
-    secLink.setAttribute("id" , `#${sections.id}L`)
-    secLink.setAttribute("class" , `navEl`)
+    secLink.setAttribute("href" , `#${sections.id}`);
+    secLink.setAttribute("id" , `#${sections.id}L`);
+    secLink.setAttribute("class" , `navEl`);
+    secLink.scrollIntoView( {behavior : "smooth"});
     // secLink.setAttribute("class" , `navEl`)
         // append all created Li elements
-    secLink.textContent = sections.firstElementChild.textContent
-    const apLi = document.querySelector('#nav-bar').appendChild(secLink)
+    secLink.textContent = sections.firstElementChild.textContent;
+    const apLi = document.querySelector('#nav-bar').appendChild(secLink);
 });
 
 
@@ -21,8 +21,8 @@ let navL = Array.from(document.querySelectorAll('.navEl'));
         console.log(navL)
         let options = {
             root: null,
-            rootMargin: '150px',
-            threshold: 0.80
+            rootMargin: '-40% 0px -20%  0px',
+            threshold: 0
           }
                     
         let observer = new IntersectionObserver(function(entries,observer){
@@ -37,9 +37,10 @@ let navL = Array.from(document.querySelectorAll('.navEl'));
                     })
                 } else {
                     entry.target.classList.remove("active");
-                    navL.forEach( nav =>{
+                    navL.forEach(nav => {
+                        if (nav.outerText == entry.target.dataset.nav){
                         nav.classList.remove("activeNav")
-                    })
+                    }})
                 }
             });
         
