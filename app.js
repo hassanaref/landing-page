fb765653d8f9a71a9ce9f3ae71a5a081b5fb8266
hassanaref <hassan.aref159@gmail.com>
2020-10-16 19:43:14 +0200

    t

--- app.js
+++ app.js
@@ -1,54 +1,69 @@
-        //selecting all sections
-const sections = Array.from(document.querySelectorAll('.sections'));
-   console.log(`there is ${sections.length} sections`);
-    console.log(sections)
-        //loop sections to nav bar
-sections.forEach(sections => {  
-    const secLink = document.createElement('a');
-    secLink.setAttribute("href" , `#${sections.id}`);
-    secLink.setAttribute("id" , `#${sections.id}L`);
-    secLink.setAttribute("class" , `navEl`);
-    secLink.scrollIntoView( {behavior : "smooth"});
-    // secLink.setAttribute("class" , `navEl`)
-        // append all created Li elements
-    secLink.textContent = sections.firstElementChild.textContent;
-    const apLi = document.querySelector('#nav-bar').appendChild(secLink);
+ //selecting all sections
+ const sections = Array.from(document.querySelectorAll('.sections'));
+ console.log(`there is ${sections.length} sections`);
+  console.log(sections)
+      //loop sections to nav bar
+sections.forEach((sections, index) => {  
+  const secLink = document.createElement('a');
+  secLink.setAttribute("id" , `#${sections.id}L`);
+  secLink.setAttribute("class" , `navEl`);
+  secLink.setAttribute("section-id" , `div${index+1}`);
+  secLink.scrollIntoView( {behavior : "smooth"});
+  // secLink.setAttribute("class" , `navEl`)
+      // append all created Li elements
+  secLink.textContent = sections.firstElementChild.textContent;
+  const apLi = document.querySelector('#nav-bar').appendChild(secLink);
 });
 
 
-        //highlighting viewed section by observer
+      //highlighting viewed section by observer
 let navL = Array.from(document.querySelectorAll('.navEl'));
-        console.log(navL)
-        let options = {
-            root: null,
-            rootMargin: '-40% 0px -20%  0px',
-            threshold: 0
-          }
-                    
-        let observer = new IntersectionObserver(function(entries,observer){
-            
-            entries.forEach( entry => {
-                if(entry.isIntersecting){
-                    entry.target.classList.add("active");
-                    navL.forEach( nav => {
-                        if (nav.outerText == entry.target.dataset.nav){
-                            nav.classList.add("activeNav")
-                        }
-                    })
-                } else {
-                    entry.target.classList.remove("active");
-                    navL.forEach(nav => {
-                        if (nav.outerText == entry.target.dataset.nav){
-                        nav.classList.remove("activeNav")
-                    }})
-                }
-            });
-        
-        }, options);
+      console.log(navL)
+      let options = {
+          root: null,
+          rootMargin: '-40% 0px -20%  0px',
+          threshold: 0
+        }
+                  
+      let observer = new IntersectionObserver(function(entries,observer){
+          
+          entries.forEach( entry => {
+              if(entry.isIntersecting){
+                  entry.target.classList.add("active");
+                  navL.forEach( nav => {
+                      if (nav.outerText == entry.target.dataset.nav){
+                          nav.classList.add("activeNav")
+                      }
+                  })
+              } else {
+                  entry.target.classList.remove("active");
+                  navL.forEach(nav => {
+                      if (nav.outerText == entry.target.dataset.nav){
+                      nav.classList.remove("activeNav")
+                  }})
+              }
+          });
+      
+      }, options);
 
-        
+      
 sections.forEach( section => {
 
-    observer.observe(section);
+  observer.observe(section);
 
+})
+        // making scroll smooth
+document.querySelectorAll('.navEl').forEach(section => {
+  section.addEventListener('click', (evt) => {
+      console.log(evt.target.getAttribute('section-id'))
+      let element = document.getElementById(evt.target.getAttribute('section-id'));
+      let topPos = element.getBoundingClientRect().top + window.scrollY;
+
+      window.scrollTo({
+           top: topPos - 160,
+           behavior: "smooth"
+      });
+
+      console.log(topPos)
+  })
 })
